20a67847c9ff5e7559cc843213dafe9963aa3d87
“Vladislav <vladlinklink@gmail.com>
2023-06-29 18:11:09 +0300

    hw

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,13 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof state => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+            case 'CHANGE_LOADING' :
+                return (
+            { isLoading : !state.isLoading}
+                )
         default:
             return state
     }
